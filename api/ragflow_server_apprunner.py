98d0f3b7fda18a6544cc481c30facc2e65cd6df6
“Raj” <rraj19@gmail.com>
2025-04-02 16:01:36 +0530

    refactoring for quick init

--- api/ragflow_server_apprunner.py
+++ api/ragflow_server_apprunner.py
@@ -9,12 +9,39 @@ import time
 import traceback
 from werkzeug.serving import run_simple
 from api.db.runtime_config import RuntimeConfig
-from api.apps import app
+
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import uuid
 from rag.utils.redis_conn import RedisDistributedLock
 from flask import jsonify
+from flask_session import Session
+from flask_login import LoginManager
+from api.utils import CustomJSONEncoder, commands
+from api.utils.api_utils import server_error_response
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True, max_age=2592000)
+app.url_map.strict_slashes = False
+app.json_encoder = CustomJSONEncoder
+app.errorhandler(Exception)(server_error_response)
+
+## convince for dev and debug
+# app.config["LOGIN_DISABLED"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["MAX_CONTENT_LENGTH"] = int(
+    os.environ.get("MAX_CONTENT_LENGTH", 1024 * 1024 * 1024)
+)
+
+Session(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+commands.register_commands(app)
+
 
 stop_event = threading.Event()
 
@@ -31,9 +58,25 @@ def update_progress():
         except Exception:
             logging.exception("update_progress exception")
 
+def lazy_init():
+    from api.db.db_models import init_database_tables as init_web_db
+    from api.db.init_data import init_web_data
+
+    init_web_db()
+    init_web_data()
+
+    thread = ThreadPoolExecutor(max_workers=1)
+    thread.submit(update_progress)
+
 @app.route('/healthz', methods=['GET'])
 def health_check():
     return jsonify({'status': 'ok'}), 200
+
+@app.route('/lazy', methods=['GET'])
+def lazy():
+    lazy_init()
+    return jsonify({'status': 'ok'}), 200
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -50,23 +93,6 @@ if __name__ == '__main__':
             use_reloader=RuntimeConfig.DEBUG,
             use_debugger=RuntimeConfig.DEBUG,
         )
-
-        from api.db.runtime_config import RuntimeConfig
-        from api.db.services.document_service import DocumentService
-        from api import utils
-
-        from api.db.db_models import init_database_tables as init_web_db
-        from api.db.init_data import init_web_data
-        
-        
-        
-        from rag.utils.redis_conn import RedisDistributedLock
-
-        init_web_db()
-        init_web_data()
-
-        thread = ThreadPoolExecutor(max_workers=1)
-        thread.submit(update_progress)
     except Exception:
         traceback.print_exc()
         stop_event.set()
